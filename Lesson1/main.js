10ac69002a9d26f950e246d55a59e290bd2f2843
Kru1l <paarl0814@gmail.com>
2023-05-02 01:12:15 +0300

    processing

--- Lesson1/main.js
+++ Lesson1/main.js
@@ -1,5 +1,32 @@
-console.log('Hello!');
+let a1 = "Hello";
+let a2 = "owu";
+let a3 = "com";
+let a4 = "ua";
 
-let a = 4554;
+let b1 = 1;
+let b2 = 10;
+let b3 = -999;
+let b4 = 123;
+let b5 = 3.14;
+let b6 = 2.7;
+let b7 = 16;
 
-console.log(a);
+let c1 = true;
+let c2 = false;
+
+let full = `${a1} ${a2}` + "." + a3 + "." + a4;
+
+// let full = `My name is ${a1}, surname: ${a2} and house number is ${a3}`;
+
+console.log(full);
+
+console.log(b1);
+console.log(b2);
+console.log(b3);
+console.log(b4);
+console.log(b5);
+console.log(b6);
+console.log(b7);
+
+console.log(c1);
+console.log(c2);
