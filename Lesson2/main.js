6f1233d7b4d0500882cf2c1558c99a5b864f181c
Kru1l <paarl0814@gmail.com>
2023-05-04 19:27:04 +0300

    done Lesson2

--- Lesson2/main.js
+++ Lesson2/main.js
@@ -144,19 +144,65 @@ switch (weekday) {
         break;
     case 'Неділя':
         console.log('Сум за п`ятницею');
+        break;
+    default:
+        console.log('Не знаю, шо ти від мене хочеш.');
 }
 
 //     - Користувач вводить або має два числа.
 //         Потрібно знайти та вивести максимальне число з тих двох .
 //         Також потрібно врахувати коли введені рівні числа.
 
+let number_1 = +prompt('Введіть перше число');
+let number_2 = +prompt('Введіть друге число');
 
-//     - є змінна х, яка може прийняти будь-яке значення (стрінг, число, undefined, null  і тд включно). Напишіть код який,
-//         за допомоги  оператора || буде присвоювати змінній х значення "default"  якщо значення змінної х являється falsy (хибноподібні, тобто приводиться до false)
-//
-//
+if (number_1 > number_2) {
+    console.log(number_1);
+} else if (number_1 < number_2) {
+    console.log(number_2);
+} else if (number_1 === number_2) {
+    console.log('Числа рівні');
+}
+
+//     - є змінна b, яка може прийняти будь-яке значення (стрінг, число, undefined, null  і тд включно). Напишіть код який,
+//         за допомоги  оператора || буде присвоювати змінній b значення "default"  якщо значення змінної b являється falsy (хибноподібні, тобто приводиться до false)
+
+let b = 0;
+
+if (b === NaN || b === undefined || b === null || b === 0) {
+    b = 'default';
+}
+console.log(b);
+
 //     - з файлу arrays.js (лежить в папці 2023 plan ) взяти масив coursesAndDurationArray. За допомоги іф перевірити кожен його елемент на тривалість навчання.
 //     У випадку якщо тривалість довша за 5 місяців вивести в консоль "Супер".
+
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+if (coursesAndDurationArray[0].monthDuration > 5) {
+    console.log('JavaScript Super!');
+}
+if (coursesAndDurationArray[1].monthDuration > 5) {
+    console.log('Java Super!');
+}
+if (coursesAndDurationArray[2].monthDuration > 5) {
+    console.log('Python Super!');
+}
+if (coursesAndDurationArray[3].monthDuration > 5) {
+    console.log('QA Super!');
+}
+if (coursesAndDurationArray[4].monthDuration > 5) {
+    console.log('FullStack Super!');
+}
+if (coursesAndDurationArray[5].monthDuration > 5) {
+    console.log('Frontend Super!');
+}
 
 
 
