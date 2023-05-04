0979b46a9d11c31902fbff3f50a25e85a49e204b
Kru1l <paarl0814@gmail.com>
2023-05-04 16:29:44 +0300

    processing

--- Lesson2/main.js
+++ Lesson2/main.js
@@ -58,22 +58,99 @@ let book_6 = {
 
 // - Створити масив з 10 об'єктами, які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
 
-let users = {
-
-}
+let users = [
+    {name: 'John', username: 'John.03', password: 'carlsberg05'},
+    {name: 'Devon', username: 'ArmWrestler', password: 'champ01'},
+    {name: 'Levan', username: 'Hulk', password: 'hulk7'},
+    {name: 'Lana', username: 'Girl-#1', password: 'Lana0917'},
+    {name: 'Kris', username: 'Capitan', password: 'America01'},
+    {name: 'Jack', username: 'Sparrow', password: 'thecapitan'},
+    {name: 'Steven', username: 'Dr.Strange', password: 'wizard56'},
+    {name: 'Tony', username: 'Stark3000', password: 'mypepper'},
+    {name: 'Timo', username: 'Ball#1', password: 'pingpong'},
+    {name: 'Cristiano', username: 'CR7', password: 'goldenball'}
+]
+let userPasswords = [users[0].password, users[1].password, users[2].password, users[3].password, users[4].password, users[5].password, users[6].password, users[7].password, users[8].password, users[9].password];
+console.log(userPasswords);
 
 // Логічні розгалуження:
 //     - Є змінна х, якій ви надаєте довільне числове значення.
-//     Якщо змінна x не дорівнює нулю, виведіть 'Вірно', інакше виведіть 'Невірно'. Перевірте  скрипт при a, що дорівнює 1, 0, -3
+//     Якщо змінна x не дорівнює нулю, виведіть 'Вірно', інакше виведіть 'Невірно'. Перевірте  скрипт при x, що дорівнює 1, 0, -3
+
+let x = 0;
+
+if (x !== 0) {
+    console.log('Вірно');
+} else {
+    console.log('Невірно');
+    x = x + 1;
+}
+if (x !== 0) {
+    console.log('Вірно');
+}
+
 // - Дано змінну time яка рівна числу від 0 до 59. Потрібно написати код, який перевірить, до якої четверті години попадає число
-// (в першу, другу, третю или четверту частину години).
+// (в першу, другу, третю чи четверту частину години).
+
+let time = 44;
+
+if (time > 0 && time <= 15) {
+    console.log('First quarter');
+} else if (time > 15 && time <= 30) {
+    console.log('Second quarter');
+} else if (time > 30 && time <= 45) {
+    console.log('Third quarter')
+} else if (time > 45 && time < 60) {
+    console.log('fourth quarter');
+}
+
 // - У змінній day дано якесь число від 1 до 31. Потрібно визначити, у яку половину(декаду) місяця потрапляє це число (у першу, другу чи третю).
+
+let day = 32;
+
+if (day > 0 && day <= 11) {
+    console.log('Перша декада');
+} else if (day > 11 && day <= 21) {
+    console.log('Друга декада');
+} else if (day > 21 && day <= 31) {
+    console.log('Третя декада');
+} else {
+    console.log('Це шо за день такий?');
+}
+
 // - Скласти розклад на тиждень за домопоги switch. Користувач вводить порядковий номер дня тижня і на екрані відображається інфа що заплановано на цей день
 // (можна замість плану на день, назву дня англійською).
+
+let weekday = 'Неділя';
+
+switch (weekday) {
+    case 'Понеділок':
+        console.log('Робота');
+        break;
+    case 'Вівторок':
+        console.log('Настільний теніс');
+        break;
+    case 'Середа':
+        console.log('Наука');
+        break;
+    case 'Четвер':
+        console.log('Волейбол');
+        break;
+    case 'П`ятниця':
+        console.log('Плавання');
+        break;
+    case 'Субота':
+        console.log('Барбекю');
+        break;
+    case 'Неділя':
+        console.log('Сум за п`ятницею');
+}
+
 //     - Користувач вводить або має два числа.
 //         Потрібно знайти та вивести максимальне число з тих двох .
 //         Також потрібно врахувати коли введені рівні числа.
-//
+
+
 //     - є змінна х, яка може прийняти будь-яке значення (стрінг, число, undefined, null  і тд включно). Напишіть код який,
 //         за допомоги  оператора || буде присвоювати змінній х значення "default"  якщо значення змінної х являється falsy (хибноподібні, тобто приводиться до false)
 //
